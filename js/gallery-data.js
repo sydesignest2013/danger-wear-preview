c25738da1c31918fbfdbc74c64fef1d5582ccf34
sydesignest2013 <karolpatykpw@gmail.com>
2026-09-18 01:47:57 +0200

    Restore and sync js/gallery-data.js from Drive

---
+++ js/gallery-data.js
@@ -0,0 +1,68 @@
+window.DW_GALLERIES = {
+  "lifestyle": [
+    {
+      "src": "products/koszulki-sportowe-mesh/lifestyle/apator-torun.jpg",
+      "title": "APATOR TORUŃ"
+    },
+    {
+      "src": "products/koszulki-sportowe-mesh/lifestyle/miedz-legnica.jpg",
+      "title": "MIEDŹ LEGNICA",
+      "subtitle": "SEKCJA SPORTOWO-TURYSTYCZNA"
+    },
+    {
+      "src": "products/koszulki-sportowe-mesh/lifestyle/rakow-3w.jpg",
+      "title": "RAKÓW",
+      "subtitle": "3W"
+    }
+  ],
+  "realizacje": [
+    {
+      "src": "products/koszulki-sportowe-mesh/realizacje/01-ruch-chorzow.jpg",
+      "title": "RUCH CHORZÓW"
+    },
+    {
+      "src": "products/koszulki-sportowe-mesh/realizacje/02-slask-wroclaw.jpg",
+      "title": "ŚLĄSK WROCŁAW"
+    },
+    {
+      "src": "products/koszulki-sportowe-mesh/realizacje/03-unia-oswiecim.jpg",
+      "title": "UNIA OŚWIĘCIM"
+    },
+    {
+      "src": "products/koszulki-sportowe-mesh/realizacje/04-chemik-kedzierzyn-kozle.jpg",
+      "title": "CHEMIK KĘDZIERZYN-KOŹLE"
+    },
+    {
+      "src": "products/koszulki-sportowe-mesh/realizacje/05-gwardia-arka.jpg",
+      "title": "GWARDIA & ARKA"
+    },
+    {
+      "src": "products/koszulki-sportowe-mesh/realizacje/06-arka-gdynia.jpg",
+      "title": "ARKA GDYNIA"
+    },
+    {
+      "src": "products/koszulki-sportowe-mesh/realizacje/07-polonia-bytom.jpg",
+      "title": "POLONIA BYTOM"
+    },
+    {
+      "src": "products/koszulki-sportowe-mesh/realizacje/08-wisla-plock.jpg",
+      "title": "WISŁA PŁOCK"
+    },
+    {
+      "src": "products/koszulki-sportowe-mesh/realizacje/09-ggkm-grudziadz.jpg",
+      "title": "GGKM GRUDZIĄDZ"
+    },
+    {
+      "src": "products/koszulki-sportowe-mesh/realizacje/10-gks-tychy.jpg",
+      "title": "GKS TYCHY"
+    },
+    {
+      "src": "products/koszulki-sportowe-mesh/realizacje/11-sandecja-nowy-sacz.jpg",
+      "title": "SANDECJA NOWY SĄCZ"
+    },
+    {
+      "src": "products/koszulki-sportowe-mesh/realizacje/12-gornik-zabrze.jpg",
+      "title": "GÓRNIK ZABRZE"
+    }
+  ]
+};
